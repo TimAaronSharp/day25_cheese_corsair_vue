2a9e298d7b483be32643fb518276faeae5ec4b31
TimAaronSharp <timothyaaron.sharp@gmail.com>
2025-03-17 16:34:00 -0600

    Init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+// JS
+<script setup>
+import { computed, ref } from 'vue'
+import { AppState } from './AppState.js'
+
+
+let cheese = ref(0)
+
+</script>
+
+
+// HTML
+<template>
+  <section class="container">
+    <div class="row">
+      <div class="col-md-1">
+        <div>
+          <p>Cheese: {{ cheese }}</p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+
+// styles
+<style lang="scss">
+@import "./assets/scss/main.scss";
+</style>
